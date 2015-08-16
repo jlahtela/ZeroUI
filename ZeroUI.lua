d8fa3309cd3c5659502b7f57ba8b580166670c5e
jlahtela <jesse.lahtela@outlook.com>
2015-08-17 00:28:24 +0300

    fist changes
    entering combat and show attribute bars always

--- ZeroUI.lua
+++ ZeroUI.lua
@@ -1,1 +1,39 @@
-##test of lua
+
+ZeroUI = {}
+
+ZeroUI.name = "ZeroUI"
+
+function ZeroUI:Initialize()
+self.inCombat = IsUnitInCombat("player")
+EVENT_MANAGER:RegisterForEvent(self.name, EVENT_PLAYER_COMBAT_STATE, self.OnPlayerCombatState)
+
+PLAYER_ATTRIBUTE_BARS:ForceShow(yes)
+
+end
+
+function ZeroUI.OnAddOnLoaded(event, addonName)
+	if addonName == ZeroUI.name then
+	ZeroUI:Initialize()
+	end
+end
+
+EVENT_MANAGER:RegisterForEvent(ZeroUI.name, EVENT_ADD_ON_LOADED, ZeroUI.OnAddOnLoaded)
+
+function ZeroUI.OnPlayerCombatState(event, inCombat)
+  -- The ~= operator is "not equal to" in Lua.
+  if inCombat ~= ZeroUI.inCombat then
+    -- The player's state has changed. Update the stored state...
+    ZeroUI.inCombat = inCombat
+ 
+    -- ...and then announce the change.
+    if inCombat then
+      d("Entering combat.")
+	 ZO_PlayerAttributeBar:ResetFadeOutDelay() 
+
+    else
+      d("Exiting combat.")
+    end
+ 
+  end
+end
+
