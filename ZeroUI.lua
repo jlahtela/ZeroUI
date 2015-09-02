d859878e037d7956fbbbabb422a3b303a46c5422
jlahtela <jesse.lahtela@outlook.com>
2015-09-02 17:40:33 +0300

    version change
    version number change also some changes from menus system.

--- ZeroUI.lua
+++ ZeroUI.lua
@@ -4,6 +4,11 @@ ZeroUI = {}
 ZeroUI.name = "ZeroUI"
 
 function ZeroUI:Initialize()
+
+--ZeroUI menu to settings
+--.EVENT_MANAGER:RegisterForEvent("ZeroUI_OnLoad", EVENT_ADD_ON_LOADED, OnLoad)
+
+--if is in combat function
 self.inCombat = IsUnitInCombat("player")
 EVENT_MANAGER:RegisterForEvent(self.name, EVENT_PLAYER_COMBAT_STATE, self.OnPlayerCombatState)
 
@@ -14,8 +19,24 @@ end
 
 function ZeroUI.OnAddOnLoaded(event, addonName)
 	if addonName == ZeroUI.name then
-	ZeroUI:Initialize()
+	
+	local LAM2 = LibStub("LibAddonMenu-2.0")
+	--Menu settings starts here
+	local panelData = {
+	type = "panel",
+	name = "ZeroUI",
+	displayName = "ZeroUI",
+	author = "jlahtela",
+	version = "0.0.2",
+	slashCommand = "/ZeroUI",	--(optional) will register a keybind to open to this panel
+	registerForRefresh = true,	--boolean (optional) (will refresh all options controls when a setting is changed and when the panel is shown)
+	registerForDefaults = true,	--boolean (optional) (will set all options controls back to default values)
+	} 
+	LAM2:RegisterAddonPanel("ZeroUI_OptionsPanel", panelData)
 	end
+	
+	ZeroUI:Initialize()
+
 end
 
 EVENT_MANAGER:RegisterForEvent(ZeroUI.name, EVENT_ADD_ON_LOADED, ZeroUI.OnAddOnLoaded)
@@ -35,4 +56,7 @@ function ZeroUI.OnPlayerCombatState(event, inCombat)
  
   end
 end
+
+
+
 
